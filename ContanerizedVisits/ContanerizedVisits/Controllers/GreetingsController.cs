f70782493d0b5fefd0bc18cf752cd12bc73acc69
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-07-06 17:39:54 +0200

    Docker contenerized Net Core api example

---
+++ ContanerizedVisits/ContanerizedVisits/Controllers/GreetingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContanerizedVisits.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Greetings()
+        {
+            return "Greetings developer! I´m a contanerized API :)";
+        }
+    }
+}
