6af75898f20da339ac9f34948dbe7bf866f03f56
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-09-18 17:45:53 +0200

    adapted enum

--- TicketManagementAPI/TicketManagement.Domain/Aggregates/TicketAggregate/Ticket.cs
+++ TicketManagementAPI/TicketManagement.Domain/Aggregates/TicketAggregate/Ticket.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TicketManagement.Domain.SeedWork;
 
@@ -11,6 +12,8 @@ namespace TicketManagement.Domain.Aggregates.TicketAggregate
         public double CalculatedTotalPrice => GlobalDiscount > 0 && GlobalDiscount < 1 ? Products.Sum(p => p.FinalPrice) * GlobalDiscount : Products.Sum(p => p.FinalPrice);
         public double GlobalDiscount { get; private set; }
         public double TotalPrice { get; private set; }
+        public double CashReceived { get; private set; }
+        public double Change { get; private set; }
 
         public TicketState Status { get; private set; }
 
@@ -18,16 +21,22 @@ namespace TicketManagement.Domain.Aggregates.TicketAggregate
 
         internal Ticket()
         {
+            Status = TicketState.PENDING;
             Products = new List<Product>();
         }
 
         public void ApplyDiscountInAllProducts(double discount)
         {
+            //TODO: Specification pattern
+
+            if (Status != TicketState.BUILDING)
+                throw new Exception("Ticket must be in BUILDING state to admint discounts");
+
             if (discount <= 0)
-                throw new System.Exception(); // TODO: Throw new domain exception
+                throw new Exception(); // TODO: Throw new domain exception
 
             if (discount >= 1)
-                throw new System.Exception(); // TODO: Throw new domain exception
+                throw new Exception(); // TODO: Throw new domain exception
 
             foreach (var product in Products)
             {
@@ -37,13 +46,34 @@ namespace TicketManagement.Domain.Aggregates.TicketAggregate
 
         public void ApplyDiscount(double discount)
         {
+            //TODO: Specification pattern
+
+            if (Status != TicketState.BUILDING)
+                throw new Exception("Ticket must be in BUILDING state to admint discounts");
+
             if (discount <= 0)
-                throw new System.Exception(); // TODO: Throw new domain exception
+                throw new Exception(); // TODO: Throw new domain exception
 
             if (discount >= 1)
-                throw new System.Exception(); // TODO: Throw new domain exception
+                throw new Exception(); // TODO: Throw new domain exception
 
             GlobalDiscount = discount;
+        }
+
+        public void PayTicket(double cash)
+        {
+            if (Status != TicketState.PENDING)
+                throw new Exception("Ticket must be in PENDING state to admin paiments");
+
+            if (cash >= CalculatedTotalPrice)
+            {
+                CashReceived += cash;
+                if (CashReceived >= CalculatedTotalPrice)
+                {
+                    Change = CashReceived - CalculatedTotalPrice;
+                    Status = TicketState.PAID;
+                }
+            }
         }
 
         // TODO: ADD AND REMOVE PRODUCTS
