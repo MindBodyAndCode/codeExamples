9f09bc86c9244dea34beeec94ddf67875d9ca843
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-09-22 16:34:48 +0200

    improved contanerizedVisits api for docker curse

--- ContanerizedVisits/ContanerizedVisits/Controllers/GreetingsController.cs
+++ ContanerizedVisits/ContanerizedVisits/Controllers/GreetingsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace ContanerizedVisits.Controllers
 {
@@ -7,10 +8,33 @@ namespace ContanerizedVisits.Controllers
     public class GreetingsController : ControllerBase
     {
 
+        private string _identifier;
+
+        private string[] _names = new string[10] { "Juan", "Marta", "Pedro", "Raul", "Ramón", "Fran", "Antonio", "Javi", "Edu", "Carmine" };
+
+        public GreetingsController()
+        {
+            int rnd = new Random().Next(0, 9);
+            _identifier = _names[rnd];
+        }
+
         [HttpGet]
         public string Greetings()
         {
             return "Greetings developer! I´m a contanerized API :)";
         }
+
+        [HttpGet("who-are-u")]
+        public string Identify()
+        {
+            return "My name is " + _identifier;
+        }
+
+        [HttpGet("boom")]
+        public void Exception()
+        {
+            System.Environment.Exit(-1);
+        }
+
     }
 }
