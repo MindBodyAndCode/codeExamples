4a21791968a2e7c24b0501d5f7372929f85ff3c0
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-10-12 21:06:21 +0200

    First tests with actors

---
+++ ReactiveAkkaAPI/ReactiveAkkaAPI/Actors/PersonActor.cs
@@ -0,0 +1,32 @@
+﻿using Akka.Actor;
+using ReactiveAkkaAPI.Messages;
+using System;
+
+namespace ReactiveAkkaAPI.Actors
+{
+
+    public class PersonActor : ReceiveActor
+    {
+        private int _peopleMeet = 0;
+
+        public PersonActor()
+        {
+            Receive<VocalGreeting>(x =>
+            {
+                _peopleMeet++;
+                Console.WriteLine("I´ve met {0} people today", _peopleMeet);
+            });
+
+            Receive<Wave>(x =>
+            {
+                Context.Sender.Tell(new VocalGreeting("Hello!"));
+                Context.Sender.Tell(new Wave());
+            });
+        }
+
+
+    }
+
+}
+
+
