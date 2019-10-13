ae91f9daa5ea2c89bde319507ba01e2bf04265f2
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-10-13 20:12:46 +0200

    Stashed Actor. FSM representation with actors

---
+++ ReactiveAkkaAPI/ReactiveAkkaAPI/Actors/TurnstileActor.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using ReactiveAkkaAPI.Messages;
+
+namespace ReactiveAkkaAPI.Actors
+{
+    // Representation of a FSM (Finite State Machine) using actors model.
+    public class TurnstileActor : ReceiveActor
+    {
+        public TurnstileActor()
+        {
+            Become(Locked); // DEFAULT STATE
+        }
+
+        void Locked() // LOCKED STATE
+        {
+            // Do some logic...
+            Receive<TicketValidatedMessage>(msg => Become(UnLocked));
+            Receive<BarrierPushedMessage>(msg => { }); // Ignore
+        }
+
+        void UnLocked() // UNLOCKED STATE
+        {
+            // Do some logic...
+            Receive<TicketValidatedMessage>(msg => { }); // Ignore
+            Receive<BarrierPushedMessage>(msg => Become(Locked));
+        }
+
+    }
+}
