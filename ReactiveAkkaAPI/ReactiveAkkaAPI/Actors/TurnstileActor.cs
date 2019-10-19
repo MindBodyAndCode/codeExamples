4b0d95054761e10bfb84eaa74da4b7bc632b6408
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2019-10-19 17:50:37 +0200

    Created a FSM more complex with Akka

--- ReactiveAkkaAPI/ReactiveAkkaAPI/Actors/TurnstileActor.cs
+++ ReactiveAkkaAPI/ReactiveAkkaAPI/Actors/TurnstileActor.cs
@@ -4,6 +4,7 @@ using ReactiveAkkaAPI.Messages;
 namespace ReactiveAkkaAPI.Actors
 {
     // Representation of a FSM (Finite State Machine) using actors model.
+    // OJO! Existe otra forma más elegante de hacer una FSM. Se encuentra en la carpeta FSM
     public class TurnstileActor : ReceiveActor
     {
         public TurnstileActor()
@@ -14,7 +15,7 @@ namespace ReactiveAkkaAPI.Actors
         void Locked() // LOCKED STATE
         {
             // Do some logic...
-            Receive<TicketValidatedMessage>(msg => Become(UnLocked));
+            Receive<TicketValidatedMessage>(msg => Become(UnLocked)); // Cambio de estado
             Receive<BarrierPushedMessage>(msg => { }); // Ignore
         }
 
@@ -22,7 +23,7 @@ namespace ReactiveAkkaAPI.Actors
         {
             // Do some logic...
             Receive<TicketValidatedMessage>(msg => { }); // Ignore
-            Receive<BarrierPushedMessage>(msg => Become(Locked));
+            Receive<BarrierPushedMessage>(msg => Become(Locked)); // Cambio de estado
         }
 
     }
